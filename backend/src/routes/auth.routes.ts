1bf35957f0661e91786163df6b430a53923dbd2a
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-12-02 18:34:15 -0300

    fix:cont

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,17 +1,9 @@
 import express from "express";
-import {
-  changePassword,
-  forgetPassword,
-  login,
-  logout,
-  verifyToken,
-} from "../controllers/auth.controllers";
+import { login, logout, verifyToken } from "../controllers/auth.controllers";
 
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/forget-password", forgetPassword);
-router.post("/change-password", changePassword);
 router.post("/verifytoken", verifyToken);
 router.post("/logout", logout);
 
